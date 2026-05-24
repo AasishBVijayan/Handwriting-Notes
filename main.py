fa4711aa5b3e7c1478e73049d69ec9e9061eefdd
Aasish B Vijayan <aasishvijayan@gmail.com>
2026-05-24 23:21:04 +0530

    Add initial project files including main script, font files, and usage terms

---
+++ main.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+
+text="Hello I am AASISH"
+
+font_path = r"C:\Users\aasis\Documents\Handwriting Script\sketchy_notes\Sketchy Notes.otf"
+font_size = 32
+
+width,height=800,1000
+background=Image.new("RGB",(width,height),(255,255,255))
+draw=ImageDraw.Drwa(background)
+
+font=ImageFont.truetype(font_path,font_size)
+margin=50
+offset=50
+for line in textwrap.wrap(text,width=40):
+    draw.text((margin,offset),line,font=font,fill=(0,0,0))
+    offset+=font_size+10
+
+background.save("output.jpg")
+print("Image saved as output.jpg")
